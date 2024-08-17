1a529357dbf39e84fd177687529b88399b6a17b5
Michał Budzyński <michal@virtualdesign.pl>
2024-08-17 14:09:42 +0200

    add modifier to baracus

--- cards/a-baracus.ts
+++ cards/a-baracus.ts
@@ -24,7 +24,11 @@ customElements.define(
             }
 
             if (locationIndex === 1) {
-                alert("do modifier magic");
+                let modifier = document.createElement("a-modifier")!;
+                modifier.setAttribute("origin", this.Name);
+                modifier.setAttribute("op", "addpower");
+                modifier.setAttribute("value", "3");
+                this.appendChild(modifier);
                 yield `${this.Name} gains +3 power for being in the middle location`;
             } else {
                 yield `${this.Name} does not gain any additional power`;
