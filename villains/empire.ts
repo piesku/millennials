ccc4ebc9d8510d14a9a4a5fe2430b51ffc805e8d
Michał Budzyński <michal@virtualdesign.pl>
2024-08-20 22:01:49 +0200

    raintrooper action

--- villains/empire.ts
+++ villains/empire.ts
@@ -1,6 +1,5 @@
 import {ActorController} from "../controllers/ActorController.js";
 import {CardController} from "../controllers/CardController.js";
-import {element} from "../lib/random.js";
 import {Sprites} from "../sprites/sprites.js";
 
 customElements.define(
@@ -17,11 +16,18 @@ customElements.define(
 customElements.define(
     "storm-trooper",
     class extends CardController {
-        Name = "Raintroopers";
-        Cost = 1;
-        Power = 2;
-        Text = "";
+        Name = "Raintrooper";
+        Cost = 2;
+        Power = 1;
+        Text = `Once: +1 for each revealed ${this.Name}`;
         Sprite = Sprites.Stormtrooper;
+        override *OnReveal() {
+            const revealedCards = this.Battle.GetRevealedCards();
+            const sameNameCards = revealedCards.filter((card) => card.Name === this.Name);
+            const count = sameNameCards.length;
+            yield `There are ${count} ${this.Name} cards revealed`;
+            this.AddModifier(this, "addpower", count);
+        }
     },
 );
 
@@ -32,21 +38,21 @@ customElements.define(
         Description = "The Empire is a powerful force that seeks to control the galaxy";
 
         *StartBattle() {
-            let cards = ["darth-vader", "storm-trooper"];
-
             const deck = this.querySelector("a-deck")!;
-            for (let i = 0; i < 12; i++) {
-                const card = element(cards);
-                deck.appendChild(document.createElement(card));
+            const cardDistribution = {
+                "storm-trooper": 11,
+                "darth-vader": 1,
+            };
+
+            for (const [card, count] of Object.entries(cardDistribution)) {
+                for (let i = 0; i < count; i++) {
+                    deck.appendChild(document.createElement(card));
+                }
             }
 
             for (let i = 0; i < 3; i++) {
                 yield* this.DrawCard();
             }
         }
-
-        // override *RivalAI() {
-
-        // }
     },
 );
