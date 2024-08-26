1615c7f30dea5b12f8a08199342c8b56f042338b
Michał Budzyński <michal@virtualdesign.pl>
2024-08-26 23:10:19 +0200

    beksinski logic

--- cards/tmnt-leonardo.ts
+++ cards/tmnt-leonardo.ts
@@ -3,8 +3,20 @@ import {CardController} from "./CardController.js";
 
 export class Leonardo extends CardController {
     Name = "Young Tortoise Beksiński";
-    Cost = 4;
-    Power = 6;
-    Text = "";
+    Cost = 3;
+    Power = 3;
+    Text = "Gain +2 Power for each card you revealed this turn";
     Sprite = Sprites.Leonardo;
+
+    override *OnReveal() {
+        let points = 0;
+        for (let card of this.Battle.GetRevealedCards(this.Owner)) {
+            if (card.TurnPlayed === this.Battle.CurrentTurn) {
+                points += 1;
+            }
+        }
+
+        this.AddModifier(this, "addpower", points * 2);
+        yield `${this.Name} gains ${points * 2} power for revealing ${points} cards this turn`;
+    }
 }
