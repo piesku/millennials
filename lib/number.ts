5d02f6c54f3741800864abd3e07fafaab0203553
Stanisław Małolepszy <sta@malolepszy.org>
2024-08-26 10:07:06 +0200

    Fix lint

--- lib/number.ts
+++ lib/number.ts
@@ -6,12 +6,6 @@ export function clamp(min: number, max: number, num: number) {
     return Math.max(min, Math.min(max, num));
 }
 
-export function map_range(
-    value: number,
-    old_min: number,
-    old_max: number,
-    new_min: number,
-    new_max: number,
-) {
+export function map_range(value: number, old_min: number, old_max: number, new_min: number, new_max: number) {
     return ((value - old_min) / (old_max - old_min)) * (new_max - new_min) + new_min;
 }
