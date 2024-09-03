81726e4be51a3f420c4ebb256ab960c49a29973d
Michał Budzyński <michal@virtualdesign.pl>
2024-09-03 12:53:29 +0200

    parry hotter

--- cards/harry-potter.ts
+++ cards/harry-potter.ts
@@ -4,7 +4,21 @@ import {CardController} from "./CardController.js";
 export class HarryPotter extends CardController {
     Name = "Parry Hotter";
     Cost = 2;
-    Power = 3;
-    Text = "";
+    Power = 2;
+    Text = "If both sides here are full, +6 power";
     Sprite = Sprites.HarryPotter;
+
+    override *OnMessage(kind: Message, trace: Trace) {
+        trace.push(this);
+
+        switch (kind) {
+            case Message.TurnStarts:
+                if (this.Battle.CurrentTurn === this.TurnPlayed + 1) {
+                    yield trace.log(`${this.Owner.Name} gains 1 energy`);
+                    this.Owner.CurrentEnergy += 1;
+                    this.Owner.Element.ReRender();
+                }
+                break;
+        }
+    }
 }
