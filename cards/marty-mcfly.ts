06918d9c0c0bc9e640dec6415b049fd4eaba4568
Michał Budzyński <michal@virtualdesign.pl>
2024-09-04 20:57:10 +0200

    marty

--- cards/marty-mcfly.ts
+++ cards/marty-mcfly.ts
@@ -1,3 +1,4 @@
+import {Message, Trace} from "../messages.js";
 import {Sprites} from "../sprites/sprites.js";
 import {CardController} from "./CardController.js";
 
@@ -5,6 +6,20 @@ export class MartyMcFly extends CardController {
     Name = "Flarty McMy";
     Cost = 1;
     Power = 3;
-    Text = "";
+    Text = "Each turn your opponent doesn't play a card here, +2 Power.";
     Sprite = Sprites.MartyMcFly;
+
+    override *OnMessage(kind: Message, trace: Trace, card?: CardController): Generator<[Trace, string], void> {
+        if (kind === Message.TurnEnds && this.Location) {
+            const opponent_cards = this.Location.GetRevealedCards(this.Opponent);
+            const current_turn = this.Battle.CurrentTurn;
+            const opponent_cards_played_this_turn = opponent_cards.filter((card) => card.TurnPlayed === current_turn);
+            if (opponent_cards_played_this_turn.length === 0) {
+                yield trace.log(
+                    `${this.Name} gains 2 Power because the opponent didn't play a card to the ${this.Location.Name}`,
+                );
+                this.AddModifier(this, "addpower", 2);
+            }
+        }
+    }
 }
