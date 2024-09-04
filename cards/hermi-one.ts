53a845f4d244dc3dcf80083b41d58b9693da14c5
Michał Budzyński <michal@virtualdesign.pl>
2024-09-04 17:12:45 +0200

    hermione

--- cards/hermi-one.ts
+++ cards/hermi-one.ts
@@ -1,10 +1,22 @@
+import {Message, Trace} from "../messages.js";
 import {Sprites} from "../sprites/sprites.js";
 import {CardController} from "./CardController.js";
 
 export class Hermione extends CardController {
     Name = "Hermitwo";
-    Cost = 4;
-    Power = 6;
-    Text = "";
+    Cost = 2;
+    Power = 3;
+    Text = "Always: After you play a card here, +2 Power.";
     Sprite = Sprites.Hermione;
+
+    override *OnMessage(kind: Message, trace: Trace, card?: CardController) {
+        switch (kind) {
+            case Message.CardEntersTable:
+                if (card?.Location === this.Location && card?.Owner === this.Owner) {
+                    this.AddModifier(this, "addpower", 2);
+                    yield trace.log(`${this.Name} gains +2 Power due to ${card.Name} being played.`);
+                }
+                break;
+        }
+    }
 }
