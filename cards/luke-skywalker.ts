aff5cd91ae6281743162106f4b68317a2ecd5cef
Michał Budzyński <michal@virtualdesign.pl>
2024-09-04 21:04:07 +0200

    luke

--- cards/luke-skywalker.ts
+++ cards/luke-skywalker.ts
@@ -1,10 +1,24 @@
+import {element} from "../lib/random.js";
+import {Message, Trace} from "../messages.js";
 import {Sprites} from "../sprites/sprites.js";
 import {CardController} from "./CardController.js";
 
 export class LukeSkywalker extends CardController {
     Name = "Suke Lywalker";
-    Cost = 3;
+    Cost = 1;
     Power = 5;
-    Text = "";
+    Text = "After each turn, this moves";
     Sprite = Sprites.LukeSkywalker;
+
+    override *OnMessage(kind: Message, trace: Trace, card?: CardController): Generator<[Trace, string], void> {
+        if (kind === Message.TurnEnds && this.Location) {
+            const empty_slots = this.Battle.GetEmptySlots(this.Owner);
+            if (empty_slots.length === 0) {
+                yield trace.log(`${this.Name} has no place to move`);
+                return;
+            }
+            const random_slot = element(empty_slots);
+            yield* this.Move(trace, random_slot);
+        }
+    }
 }
