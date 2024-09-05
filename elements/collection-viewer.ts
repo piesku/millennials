11cdfaa75fec3a1005909dfe4fb85d639c07524d
Michał Budzyński <michal@virtualdesign.pl>
2024-09-05 13:49:39 +0200

    toggles in collection viewer

--- elements/collection-viewer.ts
+++ elements/collection-viewer.ts
@@ -3,14 +3,42 @@ import {clamp} from "../lib/number.js";
 import {CollectionFlag, get_collection_state} from "../storage.js";
 import {CardElement} from "./a-card.js";
 
-const GROUP_BY_COST = true;
 export class CollectionViewer extends HTMLElement {
+    private groupByCost: boolean = true;
+    private showWipCards: boolean = true;
+
     constructor() {
         super();
         this.attachShadow({mode: "open"});
     }
 
     connectedCallback() {
+        this.render();
+
+        this.shadowRoot!.addEventListener("click", (e) => {
+            let target = e.target as HTMLElement;
+            switch (target.id) {
+                case "toggle-group-by-cost":
+                    this.toggleGroupByCost();
+                    break;
+                case "toggle-show-wip-cards":
+                    this.toggleShowWipCards();
+                    break;
+            }
+        });
+    }
+
+    toggleGroupByCost() {
+        this.groupByCost = !this.groupByCost;
+        this.render();
+    }
+
+    toggleShowWipCards() {
+        this.showWipCards = !this.showWipCards;
+        this.render();
+    }
+
+    render() {
         let collection = get_collection_state();
         let all_cards: Array<CardElement> = [];
         let cards_by_cost: {[cost: number]: Array<CardElement>} = {};
@@ -23,7 +51,11 @@ export class CollectionViewer extends HTMLElement {
             card.setAttribute("type", card_type);
             card.classList.add("frontside");
 
-            if (card.Instance.IsVillain || card.Instance.Text === "") {
+            if (card.Instance.IsVillain) {
+                continue;
+            }
+
+            if (!this.showWipCards && card.Instance.Text === "") {
                 continue;
             }
 
@@ -51,7 +83,7 @@ export class CollectionViewer extends HTMLElement {
         all_cards.sort(CardElement.Compare);
 
         let content;
-        if (GROUP_BY_COST) {
+        if (this.groupByCost) {
             let cost_sections = Object.keys(cards_by_cost)
                 .sort((a, b) => parseInt(a) - parseInt(b))
                 .map((cost) => {
@@ -69,6 +101,10 @@ export class CollectionViewer extends HTMLElement {
                 <h1>
                     Card Collection (total: ${all_cards.length}, seen: ${known_cards_count}, unlocked:
                     ${owned_cards_count})
+                    <button id="toggle-group-by-cost">${this.groupByCost ? "Ungroup by Cost" : "Group by Cost"}</button>
+                    <button id="toggle-show-wip-cards">
+                        ${this.showWipCards ? "Hide WIP Cards" : "Show WIP Cards"}
+                    </button>
                 </h1>
                 ${cost_sections}
             `;
@@ -77,6 +113,10 @@ export class CollectionViewer extends HTMLElement {
                 <h1>
                     Card Collection (total: ${all_cards.length}, seen: ${known_cards_count}, unlocked:
                     ${owned_cards_count})
+                    <button id="toggle-group-by-cost">${this.groupByCost ? "Ungroup by Cost" : "Group by Cost"}</button>
+                    <button id="toggle-show-wip-cards">
+                        ${this.showWipCards ? "Hide WIP Cards" : "Show WIP Cards"}
+                    </button>
                 </h1>
                 <flex-row wrap start style="gap: 20px;"> ${all_cards.map((card) => card.outerHTML).join("")} </flex-row>
             `;
@@ -87,6 +127,9 @@ export class CollectionViewer extends HTMLElement {
                 :host {
                     display: block;
                     padding: 20px;
+                }
+                button {
+                    margin: 10px;
                 }
             </style>
             ${content}
