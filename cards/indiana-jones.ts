97ac3bd8de036af033f3a51033dd480ee4408c71
Michał Budzyński <michal@virtualdesign.pl>
2024-09-07 00:04:49 +0200

    Indiana Johnes

--- cards/indiana-jones.ts
+++ cards/indiana-jones.ts
@@ -1,10 +1,16 @@
+import {LocationElement} from "../elements/a-location.js";
 import {Sprites} from "../sprites/sprites.js";
 import {CardController} from "./CardController.js";
 
 export class IndianaJones extends CardController {
     Name = "Iowa Jones";
     Cost = 4;
-    Power = 6;
-    Text = "";
+    Power = 10;
+    Text = "You can only play this at locations where you are winning.";
     Sprite = Sprites.IndianaJones;
+
+    override CanBePlayedHere(slot: HTMLElement): boolean {
+        const location = slot.closest<LocationElement>("a-location");
+        return !!location && location.Instance.GetScore(this.Owner) > location.Instance.GetScore(this.Opponent);
+    }
 }
