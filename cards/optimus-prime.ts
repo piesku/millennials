4af6e994c60f5e956e06d6fd5b6466ea06d7c3f5
Michał Budzyński <michal@virtualdesign.pl>
2024-09-07 03:11:55 +0200

    optimus prime

--- cards/optimus-prime.ts
+++ cards/optimus-prime.ts
@@ -1,10 +1,23 @@
+import {Trace} from "../messages.js";
 import {Sprites} from "../sprites/sprites.js";
 import {CardController} from "./CardController.js";
 
 export class OptimusPrime extends CardController {
     Name = "Pessimus Dime";
     Cost = 5;
-    Power = 0;
-    Text = "";
+    Power = 14;
+    Text = "Once: Enemy cards here get +2 Power.";
     Sprite = Sprites.OptimusPrime;
+
+    override *OnReveal(trace: Trace) {
+        const enemyCards = this.Location!.GetRevealedCards(this.Opponent);
+        if (enemyCards.length === 0) {
+            yield trace.log(`No enemy cards found at ${location}`);
+            return;
+        }
+
+        for (let card of enemyCards) {
+            yield trace.log(card.AddModifier(this, "addpower", 2));
+        }
+    }
 }
