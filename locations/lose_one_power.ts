6a420b683678a579a6e39b7af88ab1083d531ae7
Stanisław Małolepszy <sta@malolepszy.org>
2024-09-08 23:51:22 +0200

    Cards here lose 1 power each turn.

---
+++ locations/lose_one_power.ts
@@ -0,0 +1,16 @@
+import {CardController} from "../cards/CardController.js";
+import {Message, Trace} from "../messages.js";
+import {LocationController} from "./LocationController.js";
+
+export class LoseOnePower extends LocationController {
+    Name = "LoseOnePower";
+    Description = "Cards here lose 1 power each turn.";
+    override *OnMessage(kind: Message, trace: Trace, card: CardController) {
+        if (kind === Message.TurnEnds) {
+            yield trace.fork(-1).log(`${this} damages all cards here`);
+            for (let card of this.GetRevealedCards()) {
+                yield trace.log(card.AddModifier(this, "addpower", -1));
+            }
+        }
+    }
+}
