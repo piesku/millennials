c313f603de0cb896594427c17a013a3ff6278f5f
Michał Budzyński <michal@virtualdesign.pl>
2024-09-08 21:54:00 +0200

    new avatars

--- cards/john-rambo.ts
+++ cards/john-rambo.ts
@@ -2,7 +2,7 @@ import {Sprites} from "../sprites/sprites.js";
 import {CardController} from "./CardController.js";
 
 export class JohnRambo extends CardController {
-    Name = "Rohn Jambo";
+    Name = "Ron Jambo";
     Cost = 5;
     Power = 0;
     Text = "";
