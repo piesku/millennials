f2c8ae915163bae7b98247de7f794256a4a40720
Stanisław Małolepszy <sta@malolepszy.org>
2024-09-08 23:31:40 +0200

    Give + cost to a random card in each player's hand.

---
+++ locations/give_cost_hand.ts
@@ -0,0 +1,23 @@
+import {CardElement} from "../elements/a-card.js";
+import {element} from "../lib/random.js";
+import {Trace} from "../messages.js";
+import {LocationController} from "./LocationController.js";
+
+export class GiveCostHand extends LocationController {
+    Name = "GiveCostHand";
+    Description = "Give +$1 cost to a random card in each player's hand.";
+    override *OnReveal(trace: Trace) {
+        {
+            let random_card = element(this.Battle.Player.Hand.querySelectorAll<CardElement>("a-card"));
+            if (random_card) {
+                yield trace.log(random_card.Instance.AddModifier(this, "addcost", 1));
+            }
+        }
+        {
+            let random_card = element(this.Battle.Villain.Hand.querySelectorAll<CardElement>("a-card"));
+            if (random_card) {
+                yield trace.log(random_card.Instance.AddModifier(this, "addcost", 1));
+            }
+        }
+    }
+}
