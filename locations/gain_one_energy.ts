31ad796256eaf254c3fcf6b577b11d58ea400ea7
Stanisław Małolepszy <sta@malolepszy.org>
2024-09-08 23:29:29 +0200

    Gain +1 energy.

---
+++ locations/gain_one_energy.ts
@@ -0,0 +1,16 @@
+import {Trace} from "../messages.js";
+import {LocationController} from "./LocationController.js";
+
+export class GainOneEnergy extends LocationController {
+    Name = "GainOneEnergy";
+    Description = "Gain +1 energy.";
+    override *OnReveal(trace: Trace) {
+        this.Battle.Player.CurrentEnergy += 1;
+        this.Battle.Player.Element.Render();
+        yield trace.log(`${this.Battle.Player} gain +1 energy`);
+
+        this.Battle.Villain.CurrentEnergy += 1;
+        this.Battle.Villain.Element.Render();
+        yield trace.log(`${this.Battle.Player} gain +1 energy`);
+    }
+}
