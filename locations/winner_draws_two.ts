d308bbcd225037bcdeb7caecc9fc1c6aa9d12e10
Stanisław Małolepszy <sta@malolepszy.org>
2024-09-08 23:54:19 +0200

    After turn 4, whoever is winning here draws 2 cards.

---
+++ locations/winner_draws_two.ts
@@ -0,0 +1,19 @@
+import {CardController} from "../cards/CardController.js";
+import {Message, Trace} from "../messages.js";
+import {LocationController} from "./LocationController.js";
+
+export class WinnerDrawsTwo extends LocationController {
+    Name = "WinnerDrawsTwo";
+    Description = "After turn 4, whoever is winning here draws 2 cards.";
+    override *OnMessage(kind: Message, trace: Trace, card: CardController) {
+        if (kind === Message.TurnEnds && this.Battle.CurrentTurn === 4) {
+            if (this.GetScore(this.Battle.Player) > this.GetScore(this.Battle.Villain)) {
+                yield* this.Battle.Player.DrawCard(trace);
+                yield* this.Battle.Player.DrawCard(trace);
+            } else if (this.GetScore(this.Battle.Player) < this.GetScore(this.Battle.Villain)) {
+                yield* this.Battle.Villain.DrawCard(trace);
+                yield* this.Battle.Villain.DrawCard(trace);
+            }
+        }
+    }
+}
