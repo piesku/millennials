58c0b12bfc87b88c56a730c71f6e12a42981f352
Michał Budzyński <michal@virtualdesign.pl>
2024-09-12 02:45:13 +0200

    kevin home alone

--- cards/john-rambo.ts
+++ cards/john-rambo.ts
@@ -1,3 +1,4 @@
+import {Message, Trace} from "../messages.js";
 import {Sprites} from "../sprites/sprites.js";
 import {CardController} from "./CardController.js";
 
@@ -5,6 +6,50 @@ export class JohnRambo extends CardController {
     Name = "Ron Jambo";
     Cost = 5;
     Power = 0;
-    Text = "";
+    Text = "Always: Has a power equal to the sum of your other cards here";
     Sprite = Sprites.JohnRambo;
+
+    override *OnMessageSelf(kind: Message, trace: Trace) {
+        switch (kind) {
+            case Message.CardEntersTable:
+                let total_power = 0;
+                let cards_here = this.Location?.GetRevealedCards(this.Owner) || [];
+                for (let card of cards_here) {
+                    if (card !== this) {
+                        total_power += card.CurrentPower;
+                    }
+                }
+
+                yield trace.log(this.AddModifier(this, "setpower", total_power));
+
+                break;
+        }
+    }
+
+    override *OnMessage(kind: Message, trace: Trace, card?: CardController) {
+        if (card?.Location !== this.Location) {
+            return;
+        }
+
+        if (card?.Owner !== this.Owner) {
+            return;
+        }
+
+        switch (kind) {
+            case Message.CardEntersTable:
+            case Message.CardLeavesTable:
+            case Message.CardMovesToLocation:
+            case Message.CardMovesFromLocation:
+                let total_power = 0;
+                let cards_here = this.Location?.GetRevealedCards(this.Owner) || [];
+                for (let card of cards_here) {
+                    if (card !== this) {
+                        total_power += card.CurrentPower;
+                    }
+                }
+                this.RemoveModifiers(this);
+                yield trace.log(this.AddModifier(this, "setpower", total_power));
+                break;
+        }
+    }
 }
--- cards/kevin-homealone.ts
+++ cards/kevin-homealone.ts
@@ -1,10 +1,51 @@
+import {Message, Trace} from "../messages.js";
 import {Sprites} from "../sprites/sprites.js";
 import {CardController} from "./CardController.js";
 
 export class KevinHomeAlone extends CardController {
     Name = "Empty House";
     Cost = 2;
-    Power = 2;
-    Text = "";
+    Power = 7;
+    Text = "Always: -6 Power if your opponent's part of the location is full";
     Sprite = Sprites.KevinHomeAlone;
+
+    override *OnMessageSelf(kind: Message, trace: Trace) {
+        switch (kind) {
+            case Message.CardEntersTable:
+            case Message.CardMovesToLocation:
+                if (this.Location?.IsFull(this.Opponent)) {
+                    this.AddModifier(this, "addpower", -6);
+                    yield trace.log(`it has -6 Power`);
+                }
+                break;
+        }
+    }
+
+    override *OnMessage(kind: Message, trace: Trace, card?: CardController) {
+        if (card?.Owner !== this.Opponent) {
+            return;
+        }
+        if (card?.Location !== this.Location) {
+            return;
+        }
+        switch (kind) {
+            case Message.CardEntersTable:
+            case Message.CardMovesToLocation:
+                if (this.Location?.IsFull(this.Opponent)) {
+                    this.AddModifier(this, "addpower", -6);
+                    yield trace.log(`${this} has -6 Power`);
+                }
+                break;
+            case Message.CardLeavesTable:
+            case Message.CardMovesFromLocation:
+                if (!this.HasModifiers(this)) {
+                    return;
+                }
+                if (!this.Location?.IsFull(this.Opponent)) {
+                    this.RemoveModifiers(this);
+                    yield trace.log(`${this} no longer has -6 Power`);
+                }
+                break;
+        }
+    }
 }
