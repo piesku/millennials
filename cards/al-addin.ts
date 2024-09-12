fd63cf4483f1888ee93059dec1c9996c4822706f
Michał Budzyński <michal@virtualdesign.pl>
2024-09-12 16:40:46 +0200

    alladin

--- cards/al-addin.ts
+++ cards/al-addin.ts
@@ -1,10 +1,19 @@
+import {Message, Trace} from "../messages.js";
 import {Sprites} from "../sprites/sprites.js";
 import {CardController} from "./CardController.js";
 
 export class Aladdin extends CardController {
     Name = "Thief of Persia";
     Cost = 3;
-    Power = 0;
-    Text = "";
+    Power = 4;
+    Text = "When this moves, +6 Power";
     Sprite = Sprites.Aladdin;
+
+    override *OnMessageSelf(kind: Message, trace: Trace) {
+        switch (kind) {
+            case Message.CardMovesToLocation:
+                yield trace.log(this.AddModifier(this, "addpower", 6));
+                break;
+        }
+    }
 }
