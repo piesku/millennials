1b3aa2cf27891fe4913ed31295f61a3616c0f0bb
Michał Budzyński <michal@virtualdesign.pl>
2024-09-12 17:53:24 +0200

    gndalffffff

--- cards/gan-dalf.ts
+++ cards/gan-dalf.ts
@@ -1,10 +1,25 @@
+import {element} from "../lib/random.js";
+import {Trace} from "../messages.js";
 import {Sprites} from "../sprites/sprites.js";
 import {CardController} from "./CardController.js";
 
 export class Gandalf extends CardController {
     Name = "White Wizard";
     Cost = 5;
-    Power = 0;
-    Text = "";
+    Power = 7;
+    Text = "Once: Transform your other cards here into random cards that cost 1 more";
     Sprite = Sprites.Gandalf;
+
+    override *OnReveal(trace: Trace) {
+        let cards_here = this.Location?.GetRevealedCards(this.Owner) || [];
+        const all_cards = this.Battle.Game.Collection.AllCardsByCost();
+        for (let card of cards_here) {
+            const cards_costing_1_more = all_cards[card.CurrentCost + 1] || [];
+            const random_card_type = element(cards_costing_1_more);
+            if (random_card_type) {
+                card.Element.setAttribute("type", random_card_type.Sprite.toString());
+                yield* card.Element.Instance.Reveal(trace, false);
+            }
+        }
+    }
 }
