d5ee537757e080d7102e3c0a7fe007127d46ca99
Michał Budzyński <michal@virtualdesign.pl>
2024-09-12 16:50:20 +0200

    neo heimdal

--- cards/ne-o.ts
+++ cards/ne-o.ts
@@ -1,10 +1,26 @@
+import {Trace} from "../messages.js";
 import {Sprites} from "../sprites/sprites.js";
 import {CardController} from "./CardController.js";
 
 export class Neo extends CardController {
     Name = "One";
     Cost = 6;
-    Power = 6;
-    Text = "";
+    Power = 9;
+    Text = "Once: Move your other cards one location to the left";
     Sprite = Sprites.Neo;
+
+    override *OnReveal(trace: Trace) {
+        const other_cards = this.Battle.GetRevealedCards(this.Owner);
+
+        for (let card of other_cards) {
+            const locations = this.Battle.Locations;
+            const current_location_index = locations.indexOf(card.Location!);
+            if (current_location_index > 0) {
+                const new_location = locations[current_location_index - 1];
+                if (new_location) {
+                    yield* card.Move(trace, new_location, this.Owner);
+                }
+            }
+        }
+    }
 }
