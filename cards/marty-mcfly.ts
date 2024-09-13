7d596c3a6e7c7b6e4e4819acea0348b1e86f3641
Michał Budzyński <michal@virtualdesign.pl>
2024-09-13 04:13:15 +0200

    mcflurry

--- cards/marty-mcfly.ts
+++ cards/marty-mcfly.ts
@@ -1,6 +1,7 @@
 import {element} from "../lib/random.js";
 import {Trace} from "../messages.js";
 import {Sprites} from "../sprites/sprites.js";
+import {CollectionFlag, save_card_state} from "../storage.js";
 import {CardController} from "./CardController.js";
 
 export class MartyMcFly extends CardController {
@@ -15,5 +16,9 @@ export class MartyMcFly extends CardController {
         const random_card = element(all_cards);
         let clone = random_card.Clone();
         yield* clone.Controller.AddToHand(this.Owner, trace);
+
+        if (save_card_state(random_card, CollectionFlag.Seen)) {
+            yield trace.Fork(1).Log(`you see ${random_card} for the first time!`);
+        }
     }
 }
