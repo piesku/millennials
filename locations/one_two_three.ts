545b040099153271255d98dbb4ffc6bdbb467729
Stanisław Małolepszy <sta@malolepszy.org>
2024-09-16 10:20:41 +0200

    Fix OneTwoThree's logic to allow 0-Cost cards to be played

--- locations/one_two_three.ts
+++ locations/one_two_three.ts
@@ -4,6 +4,6 @@ import {LocationController} from "./LocationController.js";
 export class OneTwoThree extends LocationController {
     Description = "Cards that cost 1, 2, or 3 cannot be played here.";
     override CanBePlayedHere(card: CardController) {
-        return card.CurrentCost > 3;
+        return card.CurrentCost < 1 || 3 < card.CurrentCost;
     }
 }
